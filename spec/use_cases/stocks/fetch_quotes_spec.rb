5a86e89ba32650dee0647507a57066b89f54c585
Josh Rendek <josh@bluescripts.net>
2013-12-03 01:59:35 -0500

    add another small test to cover stockquote api

--- spec/use_cases/stocks/fetch_quotes_spec.rb
+++ spec/use_cases/stocks/fetch_quotes_spec.rb
@@ -4,10 +4,16 @@ require './app/use_cases/stocks/fetch_quotes'
 describe Stocks::FetchQuotes do
   subject { Stocks::FetchQuotes }
   context "retrieving quotes" do
+    it "should call the stock quote api" do
+      s = subject.new('GOOG')
+      StockQuote::Stock.should_receive(:quote)
+      s.latest
+    end
     it "should get the latest quotes for GOOGLE" do
       s = subject.new('GOOG')
       s.latest
       expect(s.to_hash.keys.count).to eq(82)
     end
+
   end
 end
